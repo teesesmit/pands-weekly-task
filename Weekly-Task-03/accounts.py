4171ef783ec0a591a33915c0b6466f6ee71a1f6d
theresa smyth <teesesmit@hotmail.com>
2024-03-13 11:36:34 +0000

    Update weekly task 3

--- Weekly-Task-03/accounts.py
+++ Weekly-Task-03/accounts.py
@@ -1,11 +1,19 @@
 #Weekly Task 03
 #Accounts
 #reads in 10 character account number 
-#outputs account number with first 6 as X's and only last 4 digits showing
+#outputs account number with first 6 digits as X's and only last 4 digits showing
 #Author Theresa Smyth
 
-accountno = input("Please enter your 10 digit account number: ")) #enter account number 
-if len(accountno) != 10:
+#enter account number 
+accountno = input ("Please enter your 10 digit account number: ") 
 
-print (accountno[6:])
+# only allows 10 numbers
+if len(accountno) != 10:
+    print("Error: Account number has to be 10 numbers")
+
+else:
+    #replace the first 6 numers with X
+    hidden_number = 'X' * 6 + accountno[6:]
+    
+    print('Account Number:', hidden_number)
 
