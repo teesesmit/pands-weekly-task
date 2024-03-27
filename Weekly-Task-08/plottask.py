b8eebba64fa34619c24ccb272061b0fbdb1f8555
theresa smyth <teesesmit@hotmail.com>
2024-03-27 12:01:21 +0000

    Create weekly-task-08

---
+++ Weekly-Task-08/plottask.py
@@ -0,0 +1,5 @@
+#plot that displays
+#a histogram of a normal distribution of a 1000 values
+#with a mean of 5 and standard deviation of 2
+#and a plot of the function  h(x)=x3 in the range 0 to 10
+
