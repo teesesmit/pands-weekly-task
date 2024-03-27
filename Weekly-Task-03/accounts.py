8d0c67ff4b2d88ea544619a52af9c764513b969a
theresa smyth <teesesmit@hotmail.com>
2024-03-27 10:49:01 +0000

    update weekly task 3

--- Weekly-Task-03/accounts.py
+++ Weekly-Task-03/accounts.py
@@ -7,13 +7,15 @@
 #enter account number 
 accountno = input ("Please enter your 10 digit account number: ") 
 
-# only allows 10 numbers
+# only allows 10 numbers 
+#https://stackoverflow.com/questions/29656173/how-can-i-limit-the-amount-of-digits-in-an-input¸
 if len(accountno) != 10:
     print("Error: Account number has to be 10 numbers")
 
 else:
     #replace the first 6 numers with X
-    hidden_number = 'X' * 6 + accountno[6:]
+    #https://www.educative.io/answers/how-to-mask-a-credit-card-number-with-asterisks-in-python#
+    masked_char = 'X' * 6 + accountno[6:]
     
-    print('Account Number:', hidden_number)
+    print('Account Number:', masked_char)
 
