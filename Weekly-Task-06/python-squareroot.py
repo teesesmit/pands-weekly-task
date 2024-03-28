e55509db70205bac3081721148b4a098cea5b037
theresa smyth <teesesmit@hotmail.com>
2024-03-28 11:24:46 +0000

    Update to code and references

--- Weekly-Task-06/python-squareroot.py
+++ Weekly-Task-06/python-squareroot.py
@@ -1,34 +1,35 @@
 #Write a program that takes a positive floating-point number as input  
 #outputs an approximation of its square root.
 #You should create a function called <tt>sqrt</tt> that does this.
-#create your own sqrt function and not to use the built in functions x ** .5 or math.sqrt(x).
-#https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
-#https://pythonnumericalmethods.berkeley.edu/notebooks/chapter19.04-Newton-Raphson-Method.html 
+#create your own sqrt function and not to use the built in functions x ** .5 or math.sqrt(x). 
  
+#Input a positive integer
+number = float(input("Please enter a positive number: "))
+while number < 1:
+    print ("This is a positive number")
+    number = float(input ("This is not a positive number please try again: "))
 
+    #Newtons method to return a square root of a number
+    #initial guess for square root
+    #count numbers of iterations
+    #https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
 def sqrt(number):
-    #The loop continues as long as the absolute difference between 
-    #the square of the guess and the original number is greater than 0.0001. 
-    guess = number / 2
-    while abs(guess * guess - number) > 0.0001:
-        guess = (guess + number / guess) / 2
-    
-    return guess
+    x = number
+    count = 0
 
-def main():
-    # Take input from the user
-    user_query = input("Enter a positive number: ")
-    
-    # Converts the user input string to a floating-point number and assigns it to the variable
-    number = float(user_query)
+    while True:
+        count += 1
 
-     #Checks if it's positive   
-    if number > 0:
-            # Approximate the square root using the custom sqrt function from input num
-            approx_root = sqrt(number)
-            print(f"Approximate square root of {number} is: {approx_root:.1f}")
-    else:
-            print("Error: Please enter a positive number.")
+        root = 0.5 * (x + (number / x))
 
-if __name__ == "__main__":
-    main()
+        if abs(root - x) < 1:
+                break
+            #update root
+        x = root
+
+        return root
+    #round to 1 decimal place
+n = round(sqrt(number),1)
+
+print(f"The square root of {number} is approx {n}")
+        
